e44188a92603040f814fa842ebbd353149e86b37
eloimaduell <eloi@playmodes.com>
2024-11-14 08:47:43 +0100

    moved all to /src, ignore DS_Store

---
+++ src/graaflib/tree.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <graaflib/graph.h>
+#include <graaflib/types.h>
+
+#include <vector>
+
+namespace graaf {
+
+template <typename VERTEX_T, typename EDGE_T, graph_type GRAPH_TYPE_V>
+class tree {
+  using graph_t = graph<VERTEX_T, EDGE_T, GRAPH_TYPE_V>;
+  using edges_t = std::vector<edge_id_t>;
+
+ public:
+  tree(graph_t&& graph);
+
+  [[nodiscard]] vertex_id_t get_root() const { return root_; }
+
+  [[nodiscard]] const graph_t::vertices_t& get_leaves() const { return leaves_; }
+
+  [[nodiscard]] edges_t get_edges() const;
+
+  [[nodiscard]] bool empty() const { return graph_.vertex_count() == 0; }
+
+  [[nodiscard]] graph_t::vertex_t& get_vertex(vertex_id_t vertex_id) {
+    return graph_.get_vertex(vertex_id);
+  }
+  [[nodiscard]] const graph_t::vertex_t& get_vertex(
+      vertex_id_t vertex_id) const {
+    return graph_.get_vertex(vertex_id);
+  }
+
+  [[nodiscard]] graph_t::edge_t& get_edge(vertex_id_t vertex_id_lhs,
+                                          vertex_id_t vertex_id_rhs) {
+    return graph_.get_edge(vertex_id_lhs, vertex_id_rhs);
+  }
+  [[nodiscard]] const graph_t::edge_t& get_edge(
+      vertex_id_t vertex_id_lhs, vertex_id_t vertex_id_rhs) const {
+    return graph_.get_edge(vertex_id_lhs, vertex_id_rhs);
+  }
+  [[nodiscard]] graph_t::edge_t& get_edge(edge_id_t edge_id) {
+    return graph_.get_edge(edge_id);
+  }
+  [[nodiscard]] const graph_t::edge_t& get_edge(edge_id_t edge_id) const {
+    return graph_.get_edge(edge_id);
+  }
+
+  [[nodiscard]] graph_t::vertices_t get_neighbors(vertex_id_t vertex_id) const {
+    return graph_.get_neighbors(vertex_id);
+  }
+
+  std::vector<vertex_id_t> get_vertex_ids() const;
+    
+ private:
+  graph_t graph_{};
+  vertex_id_t root_{};
+  graph_t::vertices_t leaves_{};
+};
+
+template <typename V, typename E, graph_type T>
+[[nodiscard]] tree<V, E, T> tree_from_graph(
+    const graph<V, E, T>& input_graph,
+    const std::vector<edge_id_t>& tree_edges);
+
+}  // namespace graaf
+
+#include "tree.tpp"
---
+++ src/graaflib/tree.tpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <graaflib/properties/vertex_properties.h>
+
+namespace graaf {
+
+template <typename VERTEX_T, typename EDGE_T, graph_type GRAPH_TYPE_V>
+tree<VERTEX_T, EDGE_T, GRAPH_TYPE_V>::tree(graph_t&& graph)
+    : graph_{std::move(graph)} {
+  std::size_t highest_degree{0};
+  for (const auto& [vertex_id, _] : graph_.get_vertices()) {
+    const auto degree{properties::vertex_degree(graph_, vertex_id)};
+
+    if (degree > highest_degree) {
+      highest_degree = degree;
+      root_ = vertex_id;
+    }
+
+    if (degree == 1) {
+      leaves_.insert(vertex_id);
+    }
+  }
+}
+
+template <typename VERTEX_T, typename EDGE_T, graph_type GRAPH_TYPE_V>
+tree<VERTEX_T, EDGE_T, GRAPH_TYPE_V>::edges_t
+tree<VERTEX_T, EDGE_T, GRAPH_TYPE_V>::get_edges() const {
+  edges_t result{};
+
+  for (const auto& [id, edge] : graph_.get_edges()) {
+    result.push_back(id);
+  }
+
+  return result;
+}
+
+template <typename V, typename E, graph_type T>
+tree<V, E, T> tree_from_graph(const graph<V, E, T>& input_graph,
+                              const std::vector<edge_id_t>& tree_edges) {
+  graph<V, E, T> result{};
+
+  const auto add_vertex_if_missing{[&](vertex_id_t id) {
+    if (!result.has_vertex(id)) {
+      result.add_vertex(input_graph.get_vertex(id), id);
+    }
+  }};
+
+  for (const auto& [lhs_id, rhs_id] : tree_edges) {
+    add_vertex_if_missing(lhs_id);
+    add_vertex_if_missing(rhs_id);
+
+    result.add_edge(lhs_id, rhs_id, input_graph.get_edge(lhs_id, rhs_id));
+  }
+
+  return tree{std::move(result)};
+}
+
+template <typename VERTEX_T, typename EDGE_T, graph_type GRAPH_TYPE_V>
+std::vector<vertex_id_t> tree<VERTEX_T, EDGE_T, GRAPH_TYPE_V>::get_vertex_ids() const {
+    std::vector<vertex_id_t> vertex_ids;
+    vertex_ids.reserve(graph_.vertex_count());  // Reserve space for efficiency
+
+    for (const auto& [vertex_id, vertex] : graph_.get_vertices()) {
+        vertex_ids.push_back(vertex_id);
+    }
+
+    return vertex_ids;
+}
+
+}  // namespace graaf
